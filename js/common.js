b406d3ab310b51ade4283a80cb3b14cf7771d99a
159965658 <159965658@qq.com>
2018-11-22 11:06:55 +0800

    first

---
+++ js/common.js
@@ -0,0 +1,17 @@
+/**
+ * Created by wen on 2018/11/21.
+ */
+
+var html = document.getElementsByTagName('html')[0];
+if(html){
+    var w = window.innerWidth;
+    var fontSize = (w>750?750:w)/750 * 100;
+    html.style.fontSize = fontSize + 'px';
+}
+window.onload = function(){
+    window.onresize = function(){
+        var w = window.innerWidth;
+        var fontSize = (w>750?750:w)/750 * 100;
+        html.style.fontSize = fontSize + 'px';
+    }
+}
