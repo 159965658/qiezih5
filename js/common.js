7562feca9826510c6c1c718aaf1cdd2b66b9c29b
159965658 <159965658@qq.com>
2018-11-26 11:28:32 +0800

    更新页面

--- js/common.js
+++ js/common.js
@@ -5,13 +5,13 @@
 var html = document.getElementsByTagName('html')[0];
 if(html){
     var w = window.innerWidth;
-    var fontSize = (w>750?750:w)/750 * 100;
+    var fontSize = (w>1242?1242:w)/1242 * 100;
     html.style.fontSize = fontSize + 'px';
 }
 window.onload = function(){
     window.onresize = function(){
         var w = window.innerWidth;
-        var fontSize = (w>750?750:w)/750 * 100;
+        var fontSize = (w>1242?1242:w)/1242 * 100;
         html.style.fontSize = fontSize + 'px';
     }
 }
